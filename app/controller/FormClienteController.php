0ee5756b6e0a5f2ccb0d50e46fbe9fd70ecacea1
jayvi95 <74685024+jayvi95@users.noreply.github.com>
2021-01-17 13:10:07 -0600

    a revisar

--- app/controller/FormClienteController.php
+++ app/controller/FormClienteController.php
@@ -20,15 +20,17 @@ class FormClienteController extends Controller
         $this->sesionActiva();
 
         $datos = $this->sesion->get('login');
-        $conexion= new Conexion();
+        $conexion = new Conexion();
         $cantones = $conexion->obtenerConexion()->query('SELECT * from canton')->fetchAll();
         $usuarios = $conexion->obtenerConexion()->query('SELECT * from usuario')->fetchAll();
+
         $this->view('formCliente', [
             'formCliente' => Utiles::printScript('formCliente')
         ], array(
             'login' => $datos,
             'cantones' => $cantones,
             'usuarios' => $usuarios
+
         ));
     }
 
@@ -55,6 +57,7 @@ class FormClienteController extends Controller
             Excepcion::json($resultado);
         }
     }
+
 ///cambio
     public function guardar()
     {
@@ -66,8 +69,8 @@ class FormClienteController extends Controller
         $guardar_usuario = $this->modeloU->insertar($usuario);
         $errorU = $this->modeloU->error();
         if ($guardar_usuario !== null) {
-            $conexion= new Conexion();
-            $datos_consulta = $conexion->obtenerConexion()->query('SELECT idusuario FROM usuario WHERE usuario = :usuario',array(
+            $conexion = new Conexion();
+            $datos_consulta = $conexion->obtenerConexion()->query('SELECT idusuario FROM usuario WHERE usuario = :usuario', array(
                 ':usuario' => $cliente['codcliente']
             ))->fetchAll();
 
@@ -75,16 +78,15 @@ class FormClienteController extends Controller
             $resultado_guardar = $this->modelo->insertar($cliente);
             $errorc = $this->modelo->error();
             if ($resultado_guardar !== null || $errorc[0] === "00000") {
-                Excepcion::json(['mensaje' => 'cliente creado con exito', 'redireccion' => null /*'/formCliente/tablaClientes'*/]);
-            }
-            else {
+                Excepcion::json(['mensaje' => 'cliente creado con exito', 'redireccion' => '/formCliente/tablaClientes']);
+            } else {
                 Excepcion::json(array(
                     'resultado_guardar' => $errorc,
                     'error' => true,
                     'redireccion' => null
                 ));
             }
-        }else {
+        } else {
             Excepcion::json(array(
                 'resultado_guardar' => $errorU,
                 'error' => true,
@@ -93,21 +95,45 @@ class FormClienteController extends Controller
         }
     }
 
+
     public function tablaClientes()
     {
+
         $this->sesionActiva();
 
-        $datos = $this->modelo->seleccionar('*');
 
-        $this->view('tablaClientes', [
+        $conexion = new Conexion();
+        $datos = $conexion->obtenerConexion()->query('SELECT
+cliente.codcliente,
+cliente.nombrecliente,
+cliente.apellidocliente,
+cliente.dui,
+cliente.nit,
+cliente.direccion,
+cliente.telefono,
+cliente.matriculaescritura,
+canton.nombrecanton,
+usuario.usuario,
+usuario.correo
+FROM
+cliente
+INNER JOIN canton ON cliente.idcanton = canton.idcanton
+INNER JOIN usuario ON cliente.id_usuario = usuario.idusuario')->fetchAll();
+
+        $error=$conexion->obtenerConexion()->error();
+        // $datos = $this->modelo->seleccionar('*');
+
+        $this->view('tablaCliente', [
             'formCliente' => Utiles::printScript('formCliente')
         ], array(
-            'datos' => $datos
+            'datos' => $datos,
+
         ));
 
     }
 
-    public function modalEditar($id){
+    public function modalEditar($id)
+    {
         $this->isAjax();
         $this->sesionActivaAjax();
         $this->validarMetodoPeticion('GET');
@@ -116,12 +142,25 @@ class FormClienteController extends Controller
             exit(1);
         }
 
+
+        $conexion = new Conexion();
+        $cantones = $conexion->obtenerConexion()->query('SELECT * from canton')->fetchAll();
+
         $cliente = $this->modelo->seleccionar('*', array(
-            'idcliente' => $id,
+            'codcliente' => $id,
+        ));
+
+        $usuario = $this->modeloU->seleccionar('*', array(
+            'usuario' => $id,
+
+
         ));
 
         Flight::render('ajax/cliente/modal-editar', array(
             'cliente' => $cliente[0],
+            'usuario' => $usuario[0],
+            'cantones' => $cantones,
+
         ));
     }
 
@@ -135,27 +174,44 @@ class FormClienteController extends Controller
             Excepcion::json(['error' => true, 'mensaje' => 'Hubo un error interno']);
         }
 
-        $cliente_editar = $_POST['clientes'];
-
-        if ($cliente_editar['idcliente'] === '' || $cliente_editar['nombrecliente'] === ''
-            || $cliente_editar['apellidocliente'] === '' || $cliente_editar['dui'] === ''
-            || $cliente_editar['nit'] === ''|| $cliente_editar['direccion'] === ''
-            || $cliente_editar['telefono'] === ''|| $cliente_editar['id_canton'] === ''
-            || $cliente_editar['matriculaescritura'] === ''|| $cliente_editar['id_usuario'] === '') {
+        if (!isset($_POST['usuarios'])) {
             Excepcion::json(['error' => true, 'mensaje' => 'Hubo un error interno']);
         }
 
-        $id = $cliente_editar['idcliente'];
+        $cliente_editar = $_POST['clientes'];
+        $usuario_editar = $_POST['usuarios'];
 
-        unset($cliente_editar['idcliente']);
+        if ($cliente_editar['codcliente'] === '' || $cliente_editar['nombrecliente'] === ''
+            || $cliente_editar['apellidocliente'] === '' || $cliente_editar['dui'] === ''
+            || $cliente_editar['nit'] === '' || $cliente_editar['direccion'] === ''
+            || $cliente_editar['telefono'] === '' || $cliente_editar['idcanton'] === ''
+            || $cliente_editar['matriculaescritura'] === '' || $cliente_editar['id_usuario'] === '') {
+            Excepcion::json(['error' => true, 'mensaje' => 'Hubo un error interno al guardar el cliente']);
+        }else  if ($usuario_editar['idusuario'] === '' || $usuario_editar['usuario'] === ''
+            || $usuario_editar['contrasenia'] === '' || $usuario_editar['correo'] === ''
+            || $usuario_editar['tipo'] === '') {
+            Excepcion::json(['error' => true, 'mensaje' => 'Hubo un error interno al guardar el usuario']);
+        }
+
+
+
+        $id = $cliente_editar['codcliente'];
+
+        unset($cliente_editar['codcliente']);
 
         $resultado = $this->modelo->actualizar($cliente_editar, array(
-            'idcliente' => $id,
+            'codcliente' => $id,
+
+        ));
+
+        $resultado2 = $this->modeloU->actualizar($usuario_editar, array(
+            'usuario' => $id,
+
         ));
         //Excepcion::json($resultado);
-        if ($resultado !== 0) {
+        if ($resultado !== 0 && $resultado2 !== 0) {
             Excepcion::json(['error' => false,
-                'mensaje' => 'Cliente Editado', ]);
+                'mensaje' => 'Cliente Editado',]);
 
         } else {
             if ($this->modelo->error()[2] !== null) {
---
+++ public/js/tablaCliente.js
@@ -0,0 +1,166 @@
+function cargarModalEditar(id) {
+    $('#modal-content-body').load(`/form-cliente/modal-editar/${id}`);
+}
+
+$(document).on('click', '#btn_editar_cliente', function() {
+    let id = $(this).attr('data-id');
+    cargarModalEditar(id);
+    $('#modal_acciones_cliente').modal('show');
+})
+
+$(document).on('click', '#btn_eliminar_cliente', function() {
+    let idcliente = $(this).attr('data-id');
+    validarClienteEliminar(idcliente)
+})
+
+function validarClienteEditar() {
+
+    //objeto de cuenta
+    let cliente = {
+        codcliente: $('#btn_editar').data('codcliente'),
+        nombrecliente:$('#nombrecliente').val(),
+        apellidocliente: $('#apellidocliente').val(),
+        dui: $('#dui').val(),
+        nit: $('#nit').val(),
+        direccion: $('#direccion').val(),
+        telefono: $('#telefono').val(),
+        idcanton: $('#idcanton').val(),
+        matriculaescritura: $('#matriculaescritura').val(),
+        id_usuario: $('#id_usuario').val(),
+    }
+
+    let usuario={
+        idusuario: cliente.id_usuario,
+
+            usuario: $('#usuario').val(),
+            contrasenia: $('#contrasenia').val(),
+            correo: $('#correo').val(),
+        tipo: $('#tipo').val(),
+    }
+  //  log(cliente);
+    // casos de error
+    if (cliente.nombrecliente.length === 0) {
+        validarCampo('nombrecliente', true);
+        focus('nombrecliente');
+        return false;
+    }
+
+    //confirmar guardar
+    Swal.fire({
+        title: 'Atención',
+        text: "¿Esta seguro de editar este cliente?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#6777ef',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.value) {
+            editarCliente(cliente,usuario);
+        } else {
+            focus('nombrecliente');
+        }
+    })
+}
+
+function validarClienteEliminar(idcliente) {
+
+    log(idcliente);
+
+    //confirmar eliminar
+    Swal.fire({
+        title: 'Atención',
+        text: "¿Esta seguro de eliminar este cliente?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#6777ef',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.value) {
+            eliminarCliente(idcliente);
+        } else {
+            console.log('cliente no eliminado')
+        }
+    })
+}
+
+function editarCliente(clientes,usuarios) {
+    $.post('/form-cliente/editar', { clientes,usuarios }, function(data) {
+        console.log(data)
+        if (!data.error) {
+            Swal.fire({
+                title: 'Exito',
+                text: data.mensaje,
+                icon: 'success',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+                $('#modal_acciones_cliente').modal('hide');
+                location.href = '/formCliente/tablaClientes'
+            })
+        } else {
+            Swal.fire({
+                title: 'Error',
+                text: data.mensaje,
+                icon: 'warning',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+                $('#modal_acciones_cliente').modal('hide');
+            })
+        }
+    });
+}
+
+function eliminarCliente(idcliente) {
+    $.post('/form-cliente/eliminar', { idcliente }, function(data) {
+        console.log(data)
+        if (!data.error) {
+            Swal.fire({
+                title: 'Exito',
+                text: data.mensaje,
+                icon: 'success',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+                location.href = data.redireccion
+            })
+        } else {
+            Swal.fire({
+                title: 'Error',
+                text: data.mensaje,
+                icon: 'warning',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+            })
+        }
+    });
+}
+
+$(document).ready((event) => {
+
+
+    /*
+     * Eventos
+     */
+
+
+
+
+    $(document).on('click', '#btn_editar_cliente', function() {
+        let id = $(this).attr('data-id');
+        cargarModalEditar(id);
+        $('#modal_acciones_cliente').modal('show');
+    })
+
+
+    $(document).on("click", "#btn_editar", function(e) {
+        $('#btn_editar').blur();
+        validarClienteEditar();
+    });
+
+});
